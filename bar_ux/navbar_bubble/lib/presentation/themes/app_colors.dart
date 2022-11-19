7c76722797e87892798ef4bf647df43962eab50a
Fred Grott <fred.grott@gmail.com>
2022-11-19 13:59:14 -0600

    corrections
    corrections

--- bar_ux/navbar_bubble/lib/presentation/themes/app_colors.dart
+++ bar_ux/navbar_bubble/lib/presentation/themes/app_colors.dart
@@ -11,12 +11,3 @@ import 'package:flutter/material.dart';
 const Color primaryBrandKeyColor = Color(0xffcca0a4);
 const Color secondaryBrandKeyColor = Color(0xffddcfcf);
 const Color tertiaryBrandKeyColor = Color(0xfff77f7f);
-
-const Color primaryPrimaryColor = Color(0xffae9e9f);
-const Color secondarySecondaryColor = Color(0xffe2c0c0);
-const Color tertiaryTertiaryColor = Color(0xfff26060);
-
-const Color primaryPrimaryPrimaryColor = Color(0xfff260a6);
-//
-// ignore: prefer-correct-identifier-length
-const Color secondarySecondarySecondaryColor = Color(0xffd360f2);
